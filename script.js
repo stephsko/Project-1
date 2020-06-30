39dda855982978e83c63835d3de6788d3c8a36e4
Robert Alex Crouch <acrouch06@gmail.com>
2020-06-30 11:37:23 -0500

    edited spoonacular query url

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var apiKey = "73753e2422a4438c8dcb28a382d66b82";
 var ingredient = "cheese";
 
-queryURL = "https://api.spoonacular.com/recipes/information?apiKey=" + apiKey + "&query=" + ingredient + "&number=2";
+queryURL = "https://api.spoonacular.com/recipes/search?apiKey=" + apiKey + "&query=" + ingredient + "&number=3";
 
 $.ajax({
     url: queryURL,
