08fea467295917ab7c3bd37a27edafe046575de5
Robert Alex Crouch <acrouch06@gmail.com>
2020-07-02 16:36:31 -0500

    Populated Restaurant Modal

--- script.js
+++ script.js
@@ -8,10 +8,15 @@ var modalDescription = $("#modalDescription");
 var modalLink = $("#modalLink");
 var modalDescription2 = $("#modalDescription2");
 
+modalImage.css("text-align", "center");
+$("#modalLinkDiv").css({"text-align": "center", "margin": "5px 0"});
+modalTitle.css("text-align", "center");
+
 function populateRecipeModal(response, button) {
     modalTitle.empty();
     modalImage.empty();
     modalDescription.empty();
+    modalDescription2.empty();
 
     var id = 0;
 
@@ -35,15 +40,14 @@ function populateRecipeModal(response, button) {
     modalImageEl.attr("src", "https://spoonacular.com/recipeImages/" + response.results[id - 1].id + "-90x90." + fileExtension);
     
 
-    modalTitle.text(response.results[id - 1].title);
+    modalTitle.html((response.results[id - 1].title).bold());
     modalImage.prepend(modalImageEl);  
-    modalDescription.text("Ready in: " + response.results[id -1].readyInMinutes + " minutes.");
-    modalDescription2.text("Serves: " + response.results[id - 1].servings);
+    modalDescription.text("Ready in " + response.results[id -1].readyInMinutes + " minutes.");
+    modalDescription2.text("Serves " + response.results[id - 1].servings);
     modalLink.attr("href", recipeURL).text("Recipe!");
-    
+    modalLink.attr("target", "_blank");
     
 }
-
 
 recipeBtn.on("click", function(){
     var ingredientSearch = (searchEl.val());
@@ -76,11 +80,53 @@ recipeBtn.on("click", function(){
         $('#Result1, #Result2, #Result3, #Result4, #Result5').on("click", function(){
             populateRecipeModal(response, event.currentTarget);
             $('.modal').modal('show'); 
-            
+            $('.modal').on('shown.bs.modal', function () {
+                $('#myInput').trigger('focus')
+              })
         })
 
     })
 })
+
+function populateRestModal(response, button){
+    modalTitle.empty();
+    modalImage.empty();
+    modalDescription.empty();
+    modalDescription2.empty();
+
+    var id = 0;
+
+    if (button.id === "Result1") {
+        id = 1;
+    } else if (button.id === "Result2") {
+        id = 2;
+    } else if (button.id === "Result3") {
+        id = 3;
+    } else if (button.id === "Result4") {
+        id = 4;
+    } else if (button.id === "Result5") {
+        id = 5;
+    }
+
+    var restURL = response.businesses[id - 1].url;
+    var restImageEl = $("<img src='' alt='' id='restaurantImage" + id + "' style= 'height: 90px; width: 90px; border-radius: 50%; margin: 5px;'>");
+    var addressStr = "Address: ";
+    var phoneStr = "Phone Number: ";
+    restImageEl.attr("src", response.businesses[id - 1].image_url);
+
+    modalTitle.text(response.businesses[id - 1].name);
+    
+    modalImage.prepend(restImageEl);
+
+    modalDescription.html(addressStr.bold() + response.businesses[id - 1].location.display_address[0]);
+    modalDescription.append($("<br>"));
+    modalDescription.append(response.businesses[id - 1].location.display_address[1]);
+
+    modalLink.attr("href", restURL).text("Yelp Page");
+    modalLink.attr("target", "_blank");
+
+    modalDescription2.html(phoneStr.bold() + response.businesses[id - 1].phone);
+}
 
 restaurantBtn.on("click", function () {
     var yelpApiKey = "gHLb0y1DUUd4SfZtMn8zxnMMFKbzRHu76kPiYmq6YRxho6kU4EY-5c_9OF9k-2jUnl8MbrJk-9V-jijay4pKZQRyaFnxcfB8ufplveLn5pYWthT-WAtN_9bkUGf7XnYx";
@@ -113,10 +159,15 @@ restaurantBtn.on("click", function () {
             $("#Result" + i).prepend($(restaurantImage));
         }
 
+        $('#Result1, #Result2, #Result3, #Result4, #Result5').on("click", function(){
+            populateRestModal(response, event.currentTarget);
+            $('.modal').modal('show'); 
+            $('.modal').on('shown.bs.modal', function () {
+                $('#myInput').trigger('focus')
+              })
+        })
     })
 
 })
 
-$('.modal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
+
