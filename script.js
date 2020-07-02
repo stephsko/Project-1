65e1215ba6707de5f77b3ef8270f383ba389a253
Robert Alex Crouch <acrouch06@gmail.com>
2020-07-02 17:21:32 -0500

    added geolocation request

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var modalLink = $("#modalLink");
 var modalDescription2 = $("#modalDescription2");
 
 modalImage.css("text-align", "center");
-$("#modalLinkDiv").css({"text-align": "center", "margin": "5px 0"});
+$("#modalLinkDiv").css({ "text-align": "center", "margin": "5px 0" });
 modalTitle.css("text-align", "center");
 
 function populateRecipeModal(response, button) {
@@ -32,29 +32,29 @@ function populateRecipeModal(response, button) {
         id = 5;
     }
 
-    var recipeURL = response.results[id-1].sourceUrl;
+    var recipeURL = response.results[id - 1].sourceUrl;
 
     var modalImageEl = $("<img src='' alt='' id='" + button.id + "' style='border-radius: 50%; margin: 5px;'>");
     var fileExtension = response.results[id - 1].image.split(".").pop();
 
     modalImageEl.attr("src", "https://spoonacular.com/recipeImages/" + response.results[id - 1].id + "-90x90." + fileExtension);
-    
+
 
     modalTitle.html((response.results[id - 1].title).bold());
-    modalImage.prepend(modalImageEl);  
-    modalDescription.text("Ready in " + response.results[id -1].readyInMinutes + " minutes.");
+    modalImage.prepend(modalImageEl);
+    modalDescription.text("Ready in " + response.results[id - 1].readyInMinutes + " minutes.");
     modalDescription2.text("Serves " + response.results[id - 1].servings);
     modalLink.attr("href", recipeURL).text("Recipe!");
     modalLink.attr("target", "_blank");
-    
+
 }
 
-recipeBtn.on("click", function(){
+recipeBtn.on("click", function () {
     var ingredientSearch = (searchEl.val());
 
     var spoonApiKey = "73753e2422a4438c8dcb28a382d66b82";
     var spoonQueryURL = "https://api.spoonacular.com/recipes/search?apiKey=" + spoonApiKey + "&query=" + ingredientSearch + "&offset=1&number=5";
-   
+
     $("#searchResults").attr("style", "display: block;");
     $.ajax({
         url: spoonQueryURL,
@@ -77,18 +77,18 @@ recipeBtn.on("click", function(){
             $("#Result" + i).prepend($(recipeImage));
         }
 
-        $('#Result1, #Result2, #Result3, #Result4, #Result5').on("click", function(){
+        $('#Result1, #Result2, #Result3, #Result4, #Result5').on("click", function () {
             populateRecipeModal(response, event.currentTarget);
-            $('.modal').modal('show'); 
+            $('.modal').modal('show');
             $('.modal').on('shown.bs.modal', function () {
                 $('#myInput').trigger('focus')
-              })
+            })
         })
 
     })
 })
 
-function populateRestModal(response, button){
+function populateRestModal(response, button) {
     modalTitle.empty();
     modalImage.empty();
     modalDescription.empty();
@@ -115,12 +115,17 @@ function populateRestModal(response, button){
     restImageEl.attr("src", response.businesses[id - 1].image_url);
 
     modalTitle.text(response.businesses[id - 1].name);
-    
-    modalImage.prepend(restImageEl);
 
-    modalDescription.html(addressStr.bold() + response.businesses[id - 1].location.display_address[0]);
-    modalDescription.append($("<br>"));
-    modalDescription.append(response.businesses[id - 1].location.display_address[1]);
+    modalImage.prepend(restImageEl);
+    
+    modalDescription.append(addressStr.bold());
+
+    for (var i = 0; i < response.businesses[id - 1].location.display_address.length; i++){
+        modalDescription.append(response.businesses[id - 1].location.display_address[i]);
+        console.log(response.businesses[id - 1].location.display_address[i]);
+        modalDescription.append($("<br>"));
+        // modalDescription.append(response.businesses[id - 1].location.display_address[i]);
+    }
 
     modalLink.attr("href", restURL).text("Yelp Page");
     modalLink.attr("target", "_blank");
@@ -129,42 +134,53 @@ function populateRestModal(response, button){
 }
 
 restaurantBtn.on("click", function () {
-    var yelpApiKey = "gHLb0y1DUUd4SfZtMn8zxnMMFKbzRHu76kPiYmq6YRxho6kU4EY-5c_9OF9k-2jUnl8MbrJk-9V-jijay4pKZQRyaFnxcfB8ufplveLn5pYWthT-WAtN_9bkUGf7XnYx";
-    var yelpSearch = (searchEl.val());
-    var yelpQueryURL = "https://api.yelp.com/v3/businesses/search?term=" + yelpSearch + "&limit=5&location=Austin,TX";
+    var userLongitude;
+    var userLatitude;
 
-    $("#searchResults").attr("style", "display: block;");
-    $.ajax({
-        url: "https://cors-ut-bootcamp.herokuapp.com/" + yelpQueryURL,
-        method: "GET",
-        headers: {
-            "Authorization": "Bearer " + yelpApiKey
-        }
-    }).then(function (response) {
-        console.log(response);
+    navigator.geolocation.getCurrentPosition(function (position) {
+        userLongitude = position.coords.longitude;
+        userLatitude = position.coords.latitude;
 
-        for (var i = 1; i <= response.businesses.length; i++) {
-            if (yelpSearch == "") {
-                $("#searchResults").attr("style", "display: none;");
-                break;
+
+
+
+        var yelpApiKey = "gHLb0y1DUUd4SfZtMn8zxnMMFKbzRHu76kPiYmq6YRxho6kU4EY-5c_9OF9k-2jUnl8MbrJk-9V-jijay4pKZQRyaFnxcfB8ufplveLn5pYWthT-WAtN_9bkUGf7XnYx";
+        var yelpSearch = (searchEl.val());
+        var yelpQueryURL = "https://api.yelp.com/v3/businesses/search?term=" + yelpSearch + "&latitude=" + userLatitude + "&longitude=" + userLongitude + "&limit=5";
+
+        $("#searchResults").attr("style", "display: block;");
+        $.ajax({
+            url: "https://cors-ut-bootcamp.herokuapp.com/" + yelpQueryURL,
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + yelpApiKey
             }
-            
-            var restaurantImage = $("<img src='' alt='' id='restaurantImage" + i + "' style= 'height: 90px; width: 90px; border-radius: 50%; margin: 5px;'>");
+        }).then(function (response) {
+            console.log(response);
+
+            for (var i = 1; i <= response.businesses.length; i++) {
+                if (yelpSearch == "") {
+                    $("#searchResults").attr("style", "display: none;");
+                    break;
+                }
+
+                var restaurantImage = $("<img src='' alt='' id='restaurantImage" + i + "' style= 'height: 90px; width: 90px; border-radius: 50%; margin: 5px;'>");
 
 
-            restaurantImage.attr("src", response.businesses[i - 1].image_url);
+                restaurantImage.attr("src", response.businesses[i - 1].image_url);
 
 
-            $("#Result" + i).text(response.businesses[i - 1].name);
-            $("#Result" + i).prepend($(restaurantImage));
-        }
+                $("#Result" + i).text(response.businesses[i - 1].name);
+                $("#Result" + i).prepend($(restaurantImage));
+            }
 
-        $('#Result1, #Result2, #Result3, #Result4, #Result5').on("click", function(){
-            populateRestModal(response, event.currentTarget);
-            $('.modal').modal('show'); 
-            $('.modal').on('shown.bs.modal', function () {
-                $('#myInput').trigger('focus')
-              })
+            $('#Result1, #Result2, #Result3, #Result4, #Result5').on("click", function () {
+                populateRestModal(response, event.currentTarget);
+                $('.modal').modal('show');
+                $('.modal').on('shown.bs.modal', function () {
+                    $('#myInput').trigger('focus')
+                })
+            })
         })
     })
 
